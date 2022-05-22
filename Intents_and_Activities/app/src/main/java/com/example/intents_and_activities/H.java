21c87d69fab37a7e089fe9c622f0e2db175ac6a9
Fatima <iamfaatima@gmail.com>
2022-05-22 17:01:23 +0500

    second commit

---
+++ Intents_and_Activities/app/src/main/java/com/example/intents_and_activities/H.java
@@ -0,0 +1,14 @@
+package com.example.intents_and_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class H extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h);
+    }
+}
--- Intents_and_Activities/app/src/main/java/com/example/intents_and_activities/Learn.java
+++ Intents_and_Activities/app/src/main/java/com/example/intents_and_activities/Learn.java
@@ -2,13 +2,126 @@ package com.example.intents_and_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Learn extends AppCompatActivity {
+    Button a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
     }
+        public void A(View view) {
+            Intent intent = new Intent(Learn.this, A.class);
+            startActivity(intent);
+        }
+        public void B(View view) {
+            Intent intent = new Intent(Learn.this, B.class);
+            startActivity(intent);
+        }
+        public void C(View view) {
+            Intent intent = new Intent(Learn.this, C.class);
+            startActivity(intent);
+        }
+        public void D(View view) {
+            Intent intent = new Intent(Learn.this, D.class);
+            startActivity(intent);
+        }
+        public void E(View view) {
+            Intent intent = new Intent(Learn.this, E.class);
+            startActivity(intent);
+        }
+
+        public void F(View view) {
+            Intent intent = new Intent(Learn.this, F.class);
+            startActivity(intent);
+        }
+        public void G(View view) {
+            Intent intent = new Intent(Learn.this, G.class);
+            startActivity(intent);
+        }
+        public void H(View view) {
+            Intent intent = new Intent(Learn.this, H.class);
+            startActivity(intent);
+        }
+        public void I(View view) {
+            Intent intent = new Intent(Learn.this, I.class);
+            startActivity(intent);
+        }
+        public void J(View view) {
+            Intent intent = new Intent(Learn.this, J.class);
+            startActivity(intent);
+        }
+        public void K(View view) {
+            Intent intent = new Intent(Learn.this, K.class);
+            startActivity(intent);
+        }
+        public void L(View view) {
+            Intent intent = new Intent(Learn.this, L.class);
+            startActivity(intent);
+        }
+        public void M(View view) {
+            Intent intent = new Intent(Learn.this, M.class);
+            startActivity(intent);
+        }
+        public void N(View view) {
+            Intent intent = new Intent(Learn.this, N.class);
+            startActivity(intent);
+        }
+        public void O(View view) {
+            Intent intent = new Intent(Learn.this, O.class);
+            startActivity(intent);
+        }
+        public void P(View view) {
+            Intent intent = new Intent(Learn.this, P.class);
+            startActivity(intent);
+        }
+        public void Q(View view) {
+            Intent intent = new Intent(Learn.this, Q.class);
+            startActivity(intent);
+        }
+        public void Rr(View view) {
+            Intent intent = new Intent(Learn.this, Rr.class);
+            startActivity(intent);
+        }
+        public void S(View view) {
+            Intent intent = new Intent(Learn.this, S.class);
+            startActivity(intent);
+        }
+        public void T(View view) {
+            Intent intent = new Intent(Learn.this, T.class);
+            startActivity(intent);
+        }
+        public void U(View view) {
+            Intent intent = new Intent(Learn.this, U.class);
+            startActivity(intent);
+        }
+        public void V(View view) {
+            Intent intent = new Intent(Learn.this, V.class);
+            startActivity(intent);
+        }
+        public void W(View view) {
+            Intent intent = new Intent(Learn.this, W.class);
+            startActivity(intent);
+        }
+        public void X(View view) {
+            Intent intent = new Intent(Learn.this, X.class);
+            startActivity(intent);
+        }
+        public void Y(View view) {
+            Intent intent = new Intent(Learn.this, Y.class);
+            startActivity(intent);
+        }
+        public void Z(View view) {
+            Intent intent = new Intent(Learn.this, Z.class);
+            startActivity(intent);
+        }
+
+
+
 }
